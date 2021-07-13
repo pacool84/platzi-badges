490b78d8e317e51f55fa199c99a45639580e151c
Francisco López <flopez_1384@hotmail.com>
2021-07-12 22:35:31 -0500

    [ADD] Code to understanding JSX, REACT is JavaScript

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges</h1>; //__que__
+const name = 'Sebastian';
+
+//const jsx = <h1>Hello, Platzi Badges</h1>; //__que__
+//const element = React.createElement('h1', {}, 'Hello my firend REACT') // __Etiqueta__, __Atributos__, __Contenido__
+//const element = React.createElement('h1', {}, `Hello my friend ${name}`) // __Etiqueta__, __Atributos__, __Contenido con JavaScript__
+const jsx = <h1>Hello my friend {name}</h1> //__expresiones {}__
 const container = document.getElementById('app'); //__donde__
 
 //ReactDOM.render(__que__, __donde__)
-ReactDOM.render(element, container);
+ReactDOM.render(jsx, container);
