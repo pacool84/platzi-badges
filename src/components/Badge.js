7638d2b3adb4040752995a64a87d91d93d3517ac
Francisco López <flopez_1384@hotmail.com>
2021-07-12 23:14:56 -0500

    [ADD] The first component, Badge

---
+++ src/components/Badge.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import confLogo from '../images/badge-header.svg'
+
+class Badge extends React.Component {
+  render() {
+    return(
+      <div>
+        <div>
+          <img src={confLogo} alt="Logo de la conferencia"/>
+        </div>
+        <div>
+          <img src="https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon" alt="Avatar"/>
+          <h1>Francisco <br/>Lopez</h1>
+        </div>
+        <div>
+          <p>Front End Developer</p>
+          <p>@babasyop</p>
+        </div>
+        <div>
+          #platziconf
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Badge;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Badge from './components/Badge';
 
-const name = 'Sebastian';
+//const name = 'Sebastian';
 
 //const jsx = <h1>Hello, Platzi Badges</h1>; //__que__
 //const element = React.createElement('h1', {}, 'Hello my firend REACT') // __Etiqueta__, __Atributos__, __Contenido__
 //const element = React.createElement('h1', {}, `Hello my friend ${name}`) // __Etiqueta__, __Atributos__, __Contenido con JavaScript__
-const jsx = <h1>Hello my friend {name}</h1> //__expresiones {}__
+//const jsx = <h1>Hello my friend {name}</h1> //__expresiones {}__
 const container = document.getElementById('app'); //__donde__
 
 //ReactDOM.render(__que__, __donde__)
-ReactDOM.render(jsx, container);
+ReactDOM.render(<Badge />, container);
